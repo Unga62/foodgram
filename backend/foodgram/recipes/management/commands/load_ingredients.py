06237af7f4f6913bdd24a9494041d0d55a74feb5
Даниил Ильин <danil.ilin62@gmail.com>
2024-09-01 21:59:28 +0300

    Мелкие правки

--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -8,15 +8,17 @@ class Command(BaseCommand):
     help = "Loads ingredients from csv"
 
     def handle(self, *args, **kwargs):
-        with open('recipes/data/ingredients.csv',
-                  'r',
-                  encoding='utf-8'
-                  ) as file:
+        with open(
+            'recipes/data/ingredients.csv',
+            'r',
+            encoding='utf-8'
+        ) as file:
             ingredients = csv.reader(file, delimiter=',')
             for ingredient in ingredients:
                 if len(ingredient) == 2:
                     Ingredients.objects.get_or_create(
-                        name=ingredient[0], measurement_unit=ingredient[1]
+                        name=ingredient[0],
+                        measurement_unit=ingredient[1]
                     )
 
         self.stdout.write(self.style.SUCCESS('База данных успешно заполнена'))
