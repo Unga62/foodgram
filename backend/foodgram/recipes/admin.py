934ce79bf5a247e063779e068681dcab14875dd5
Даниил Ильин <danil.ilin62@gmail.com>
2024-09-01 22:08:18 +0300

    правки

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -9,9 +9,27 @@ from recipes.models import (
     Tags,
 )
 
+
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+    )
+    search_fields = ('name', 'author')
+    list_filter = ('tags',)
+
+
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'measurement_unit',
+    )
+    search_fields = ('name',)
+
+
 admin.site.register(Tags)
-admin.site.register(Ingredients)
-admin.site.register(Recipes)
+admin.site.register(Ingredients, IngredientsAdmin)
+admin.site.register(Recipes, RecipesAdmin)
 admin.site.register(ArrayIngredients)
 admin.site.register(Favorites)
 admin.site.register(ShoppingCart)
